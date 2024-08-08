de53f41ea1eac535985bb60ec3c5072ac021d0bc
Jack Webdev <jackbuda31@gmail.com>
2024-08-08 08:43:03 +0200

    done preData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,143 @@
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { CircleHelp } from "lucide-react";
+
+import React from "react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <header className="bg-white py-2 flex items-center justify-between px-8">
+        <Image src={"/image.webp"} alt="NSFAS" width={50} height={50} />
+        <Dialog>
+          <DialogTrigger className="underline text-[#A1261F] font-semibold flex items-center gap-x-2">
+            <CircleHelp /> Need Help?
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Contact Us</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </header>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <section className="flex items-center justify-center mt-20 md:mt-40">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={"/image.webp"}
+          alt="NSFAS"
+          width={500}
+          height={250}
+          className="hidden md:inline-block mx-auto"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Tabs
+          defaultValue="students"
+          className="bg-white md:w-1/2 md:mx-auto p-8 rounded-xl grid"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <span className="mb-4 font-semibold text-lg">For:</span>
+          <TabsList className="grid gap-y-2 md:flex mb-20 md:mb-4 md:gap-x-2  w-fit">
+            <TabsTrigger
+              value="students"
+              className="bg-gray-100 text-black p-2 text-xl"
+            >
+              Students
+            </TabsTrigger>
+            <TabsTrigger
+              value="aps"
+              className="bg-gray-100 text-black p-2 text-xl"
+            >
+              Accomodation Providers
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="students">
+            <div>
+              <p>
+                If you are an NSFAS funded student that qualifies for
+                accommodation allowance, please click Proceed to the housing
+                portal For first time users you will need to:
+              </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <ul className="list-disc list-inside my-4">
+                <li>Take a picture of your RSA ID</li>
+                <li>
+                  Take a picture of your Proof of registration/Student Card
+                </li>
+                <li>Take a picture of your Selfie (Picture of Face)</li>
+              </ul>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              <p>
+                Upon login you can search and apply for an accommodation of your
+                choice. If you are already residing in an accredited residence,
+                please make sure to apply in the same residence Your details
+                will be validated and processed upon funding confirmation
+              </p>
+              <div className="flex items-center gap-x-2 mt-4">
+                <Button className="bg-[#A1261F] text-white hover:bg-gray-100 hover:text-[#A1261F]">
+                  Login
+                </Button>
+                <Button className="bg-[#D6660E] text-white hover:bg-gray-100 hover:text-[#D6660E]">
+                  Register
+                </Button>
+              </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="aps">
+            {" "}
+            <div>
+              <h2>
+                Become an accredited NSFAS Accommodation Provider and list your
+                property to get first preference for accommodating NSFAS funded
+                students.
+              </h2>
+              <p>
+                Register your property with NSFAS and we will help you find the
+                right students for you student residence facility. Just a few
+                easy steps.
+              </p>
+              <ol className="list-decimal list-inside my-4">
+                <li>Register as an Accommodation Provider for a fee.</li>
+                <li>
+                  Get your student residence property(s) accredited by NSFAS for
+                  a fee.
+                </li>
+                <li>
+                  Once accredited, your property is visible to students on our
+                  student accommodation site.
+                </li>
+                <li>
+                  {" "}
+                  Lease out rooms to NSFAS students and get hassle free
+                  guaranteed rental payment.
+                </li>
+              </ol>
+              <div className="flex items-center gap-x-2 mt-4">
+                <Button className="bg-[#A1261F] text-white hover:bg-gray-100 hover:text-[#A1261F]">
+                  Login
+                </Button>
+                <Button className="bg-[#D6660E] text-white hover:bg-gray-100 hover:text-[#D6660E]">
+                  Register
+                </Button>
+              </div>
+            </div>
+          </TabsContent>
+        </Tabs>
+      </section>
+    </div>
   );
 }
